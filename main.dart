2069cd01dd2d31d3917e6ff8bd1d2c8999c66bfd
geddzz <gerardvince.lillo@unc.edu.ph>
2022-04-03 16:20:02 +0800

    Second Commit

--- main.dart
+++ main.dart
@@ -1,3 +1,23 @@
-void main() {
-  print("Hello World!");
+// Name: Gerard Vince I. Lillo
+// Course: BSIT
+// Subject: Applications Development & Emerging Technologies
+// Section: BIT322K-KTa
+// Git: https://github.com/geddzz/ADET21_ACT1_LILLO
+
+import 'dart:io';
+
+main() async {
+  var file = File("input.txt");
+  var contents;
+
+  if (await file.exists()) {
+    // Read File
+    contents = await file.readAsString();
+    print(contents);
+
+    // Write File
+    //var fileCopy = await File('output.txt').writeAsString(contents);
+    //print(await fileCopy.exists());
+    //print(await fileCopy.length());
+  }
 }
